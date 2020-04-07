d9d6fbde952d77ae90b21d96bbc2546b6af915f6
Alexander Pinto <alexander.pinto@ucsp.edu.pe>
2020-04-07 00:45:00 -0500

    Add files via upload

---
+++ Ejercicio1/CMakeFiles/ejercicio01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio01.dir/test.cpp.o"
+  "ejercicio01.pdb"
+  "ejercicio01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
